e68789309a26ac93e8e47006d36cc9482d51b938
Jonathan <jonathan20061023li@gmail.com>
2023-10-17 13:59:25 -0700

    test

--- input.py
+++ input.py
@@ -1,5 +1,31 @@
-import kivy
 from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+
+
+class SimpleApp(BoxLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.orientation = 'vertical'
+
+        self.count = 0
+        self.label = Label(text=f'Button pressed {self.count} times')
+        self.add_widget(self.label)
+
+        self.button = Button(text='Press me!')
+        self.button.bind(on_press=self.increment_count)
+        self.add_widget(self.button)
+
+    def increment_count(self, instance):
+        self.count += 1
+        self.label.text = f'Button pressed {self.count} times'
+
+
+class MyApp(App):
+    def build(self):
+        return SimpleApp()
+
+
+if __name__ == '__main__':
+    MyApp().run()
